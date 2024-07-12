cdf77cac878e9de131564f02854d621907addec9
Evgenii Istomin <ilonsisse@yandex.ru>
2024-07-12 05:50:17 +0300

    modified in line with recommendations

--- musical_site.sql
+++ musical_site.sql
@@ -1,44 +1,45 @@
 CREATE TABLE musical_genre (
-    genre VARCHAR(50) PRIMARY KEY,
+    genre_id SERIAL PRIMARY KEY, -- добавил PK на id
+    genre VARCHAR(50) UNIQUE NOT NULL, -- ограничение UNIQUE NOT NULL на поле genre
     genre_description TEXT
 );
 
 CREATE TABLE singer (
-    nickname VARCHAR(50) PRIMARY KEY,
+    singer_id SERIAL PRIMARY KEY, -- добавил PK на id
+    nickname VARCHAR(50) UNIQUE NOT NULL, -- ограничение UNIQUE NOT NULL на поле nickname
     name VARCHAR(50) NOT NULL,
     surname VARCHAR(50) NOT NULL
 );
 
-
 CREATE TABLE singer_genre (
-    singer_nickname VARCHAR(50) NOT NULL REFERENCES singer(nickname),
-    genre VARCHAR(50) NOT NULL REFERENCES musical_genre(genre),
-    PRIMARY KEY (singer_nickname, genre)
+    singer_id INTEGER NOT NULL REFERENCES singer(singer_id),
+    genre_id INTEGER NOT NULL REFERENCES musical_genre(genre_id),
+    PRIMARY KEY (singer_id, genre_id)
 );
 
 CREATE TABLE album (
     album_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    release_date DATE NOT NULL
+    release_date DATE NOT NULL CHECK (release_date >= '1900-01-01') -- ограничение на дату выхода
 );
 
 CREATE TABLE album_singer (
     album_id INTEGER NOT NULL REFERENCES album(album_id),
-    singer_nickname VARCHAR(50) NOT NULL REFERENCES singer(nickname),
-    PRIMARY KEY (album_id, singer_nickname)
+    singer_id INTEGER NOT NULL REFERENCES singer(singer_id),
+    PRIMARY KEY (album_id, singer_id)
 );
 
 CREATE TABLE track (
     track_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    duration INTERVAL NOT NULL,
+    duration INTEGER NOT NULL CHECK (duration > 0 AND duration <= 3600), -- поменял тип на INTEGER и ограничил длительность трека
     album_id INTEGER NOT NULL REFERENCES album(album_id)
 );
 
 CREATE TABLE compilation (
     compilation_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    release_year INTEGER NOT NULL
+    release_year INTEGER NOT NULL CHECK (release_year >= 1900) -- ограничение на год выпуска
 );
 
 CREATE TABLE compilation_track (
@@ -46,4 +47,3 @@ CREATE TABLE compilation_track (
     track_id INTEGER NOT NULL REFERENCES track(track_id),
     PRIMARY KEY (compilation_id, track_id)
 );
-
